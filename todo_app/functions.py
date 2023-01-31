4429d81ae2637d10dca4cecabec674fdf2b118cf
Gaveling <Gaveling@me.com>
2023-01-30 21:25:04 -0500

    changed main.py to cli.py

--- todo_app/functions.py
+++ todo_app/functions.py
@@ -1,11 +1,18 @@
-def get_todos(filepath="files_todo/todos.txt") -> object:
+FILEPATH = "files_todo/todos.txt"
+
+
+def get_todos(filepath=FILEPATH) -> object:
     """ reads a text file and returns list of todos """
     with open(filepath, 'r') as file_local:
         todos_local = file_local.readlines()
     return todos_local
 
 
-def write_todos(todos_arg, filepath="files_todo/todos.txt"):
+def write_todos(todos_arg, filepath=FILEPATH):
     """ writes the to-do list, to a file """
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
+
+
+if __name__ == "__main__":
+    print(get_todos())
